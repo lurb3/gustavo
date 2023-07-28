4a11da4e9a014ceea5770e7cdebb5dfeb9924a3e
gustavo <gustavo.gigante.s@gmail.com>
2023-07-28 23:40:41 +0100

    misc

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from "../services/auth.service";
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  isLoggedin: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.isLoggedin = this.authService.isAuthenticated();
+  }
 
 }
