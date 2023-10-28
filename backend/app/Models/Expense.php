a8c9b557bf76ac0e05b6ca6b737b1a124550f3cd
Gustavo <gustavo.gigante.s@gmail.com>
2023-10-28 21:12:01 +0100

    expenses init

---
+++ backend/app/Models/Expense.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    use HasFactory;
+
+    protected $table = 'expenses';
+
+    protected $fillable = [
+        'amount',
+        'currency',
+        'transaction_date',
+        'user_id'
+    ];
+}
